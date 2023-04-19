2c1d1aa2d3ff45b86111c9aaca2ffe66ab533b47
Ben Gao <bengao168@msn.com>
2023-04-19 20:45:44 +0800

    增加命令行开关。 完善使用指南。

--- README.md
+++ README.md
@@ -2,28 +2,40 @@
 批量下载，打包和推送docker镜像的python小工具
 
 # 为什么要写这个工具
-在使用docker的过程中，我们经常需要下载一些镜像，然后打包成tar包，然后推送到私有仓库中，这个过程是比较繁琐的，所以写了这个工具来简化这个过程。
-  对于网络环境不好的情况下，下载镜像的过程可能会很慢，所以这个工具也可以用来加快镜像移动的步骤。
-  对于没有外网访问的环境，可以先在有外网的环境中下载好镜像，然后打包成tar包，然后推送到私有仓库中。
+在使用docker的过程中，我们经常需要在一个环境中下载一些镜像，打包成tar包，拷贝到另一个环境，然后推送到私有仓库中。这个过程是比较繁琐的，所以写了这个工具来简化这个过程。
+  - 对于网络环境不好的情况下，下载镜像的过程可能会很慢，所以这个工具也可以用来加快镜像移动的速度。
+  - 对于没有外网访问的环境，可以通过本工具先在有外网的环境中下载好镜像，打包成tar包，传输到生产环境，然后本工具批量推送到私有仓库中。
 
 # 使用方法
 ## 下载,打包镜像
 运行完成如下命令后，打包的镜像保存在当前目录的image.tar.gz中
 ```
-python3 images_mover.py --pull-save
+python3 images_mover.py --pull-save --image-list-file image_list_file
 ```
 ## 推送镜像
 会默认在当前目录下寻找image.tar.gz文件，需要将上一步的打包文件image.tar.gz文件放在当前目录下。执行如下命令后，会将image.tar.gz文件中的镜像推送到私有仓库中
 ```
-python3 images_mover.py --tag-push
+python3 images_mover.py --tag-push  --registry-url 192.168.1.123:5000
 ```
 ## 下载,打包,推送镜像
 本机作为中转站，下载镜像，打包，推送到私有仓库中。中间不需要保存打包文件，传输文件。
 ```
-python3 images_mover.py --pull -tag-push
+python3 images_mover.py --pull -tag-push  --registry-url 192.168.1.123:5000 --image-list-file image_list_file
 ```
 ## 镜像列表和私有仓库
-镜像列表和私有仓库的配置在images_mover.py文件image_list和registry_url中，可以根据自己的需要进行修改。
+镜像列表和私有仓库的配置通过开关--image-list-file 和--registry-url指定，可以根据自己的需要进行输入。image_list_file中的镜像列表格式如下：
+```
+image_name1:tag1
+image_name2:tag2
+image_name3:tag3
+```
 
-## 说明
+# 注意事项
+- 需要安装如下软件：
+  - docker环境
+  - python3环境
+  - gzip/gunzip
+- 适用linux/unix系统
+
+# 说明
 代码主要由cursor输出，感谢AI助手协助。
--- images_mover.py
+++ images_mover.py
@@ -3,10 +3,14 @@ import argparse
 
 # List of Docker image to be moved to the local registry
 #image_list = ["quay.io/prometheus/node-exporter:v1.3.1", "docker.io/grafana/loki:2.4.0", "docker.io/grafana/promtail:2.4.0", "quay.io/ceph/haproxy:2.3", "quay.io/ceph/keepalived:2.1.5", "docker.io/maxwo/snmp-notifier:v1.2.1"]
-image_list = ["quay.io/prometheus/alertmanager:v0.23.0", "quay.io/prometheus/prometheus:v2.33.4", "quay.io/ceph/ceph-grafana:8.3.5"]
+#image_list = ["quay.io/prometheus/alertmanager:v0.23.0", "quay.io/prometheus/prometheus:v2.33.4", "quay.io/ceph/ceph-grafana:8.3.5"]
 # Local Registry URL
-registry_url = "10.123.123.123:5000"
+#registry_url = "10.123.123.123:5000"
 
+def read_image_list(file_path):
+    with open(file_path, "r") as f:
+        image_list = f.read().splitlines()
+    return image_list
 
 def pull_save_as_tar_gz(image_list, save_tar):
     # Loop through the list and pull each image
@@ -20,7 +24,7 @@ def pull_save_as_tar_gz(image_list, save_tar):
         subprocess.run(["gzip", "image.tar"])
 
 # file_path is in tar.gz format
-def tag_push_to_registry(image_list, registry_url, file_path):
+def tag_push_to_registry(image_list, registry_url, file_path="", project_name=None):
     if file_path != "":
         # Use subprocess to run the gunzip command and decompress the tar.gz file
         subprocess.run(["gunzip", file_path])
@@ -33,8 +37,14 @@ def tag_push_to_registry(image_list, registry_url, file_path):
 
     # Loop through the list of image names and tag and push each image to the specified registry
     for image_name in image_list:
-        # Tag the image with the specified registry URL
-        tagged_image_name = registry_url + "/" + image_name.split("/")[-1]
+        # Get the image name without the hostname
+        image_name_without_hostname = image_name.split("/")[-1]
+
+        # Tag the image with the specified registry URL, project name, and the image name without the hostname
+        if project_name != None:
+            tagged_image_name = registry_url + "/" + project_name + "/" + image_name_without_hostname
+        else:
+            tagged_image_name = registry_url + "/" + image_name_without_hostname
         subprocess.run(["docker", "tag", image_name, tagged_image_name])
 
         # Push the tagged image to the registry
@@ -45,7 +55,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--tag-push", action="store_true", help="Tag and push the images to the registry")
 parser.add_argument("--pull", action="store_true", help="Pull the images without saving them to a tar file")
 parser.add_argument("--pull-save", action="store_true", help="Pull the images and save them to a tar file")
+parser.add_argument("--project-name", type=str, help="Specify the project name for pushing the images to the registry")
+parser.add_argument("--image-list-file", type=str, help="Specify the file path for reading the image list")
+parser.add_argument("--registry-url", type=str, help="Specify local docker image registry to push to")
 args = parser.parse_args()
+
+# Check if the --image-list-file argument is set
+if not args.image_list_file and (args.pull or args.pull_save):
+    parser.error("--image-list-file is required with pulling operation")
+
+image_list = []
+if args.image_list_file:
+# Read the image list from file
+    image_list = read_image_list(args.image_list_file)
+
+if not args.registry_url and args.tag_push:
+    parser.error("--registry-url is required with pushing operation")
 
 # Pull the images and save them to a tar file if the --pull-save switch is set
 if args.pull_save:
@@ -56,13 +81,8 @@ elif args.pull:
     pull_save_as_tar_gz(image_list, False)
 
 # Tag and push the images to the registry if the --tag-push switch is set
-elif args.tag_push:
+if args.tag_push:
     if args.pull:
-        tag_push_to_registry(image_list, registry_url, "")
+        tag_push_to_registry(image_list, args.registry_url, project_name=args.project_name)
     else:
-        tag_push_to_registry(image_list, registry_url, "image.tar.gz")
-
-
-# Print an error message if no switches are set
-else:
-    print("Error: You must specify at least one switch (--tag-push, --pull, or --pull-save).")
+        tag_push_to_registry(image_list, args.registry_url, "image.tar.gz", project_name=args.project_name)
