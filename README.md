90e3f637cf9dbd147819b9753dbed4c40573eadf
Ben Gao <bengao168@msn.com>
2023-06-25 22:34:26 +0800

    支持多架构镜像的搬移

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ImagesMover是什么
-批量下载，打包和推送docker镜像的python小工具
+批量下载，打包和推送docker镜像的python小工具，提供指定多架构镜像下载，打包和推送功能。
 
 # 为什么要写这个工具
 在使用docker的过程中，我们经常需要在一个环境中下载一些镜像，打包成tar包，拷贝到另一个环境，然后推送到私有仓库中。这个过程是比较繁琐的，所以写了这个工具来简化这个过程。
@@ -10,17 +10,17 @@
 ## 下载,打包镜像
 运行完成如下命令后，打包的镜像保存在当前目录的image.tar.gz中
 ```
-python3 images_mover.py --pull-save --image-list-file image_list_file
+python3 images_mover.py --pull-save --image-list-file image_list_file --platforms linux/amd64 linux/arm64
 ```
 ## 推送镜像
 会默认在当前目录下寻找image.tar.gz文件，需要将上一步的打包文件image.tar.gz文件放在当前目录下。执行如下命令后，会将image.tar.gz文件中的镜像推送到私有仓库中
 ```
-python3 images_mover.py --tag-push  --registry-url 192.168.1.123:5000
+python3 images_mover.py --tag-push  --registry-url 192.168.1.123:5000 --image-list-file image_list_file --platforms linux/amd64 linux/arm64
 ```
 ## 下载,打包,推送镜像
 本机作为中转站，下载镜像，打包，推送到私有仓库中。中间不需要保存打包文件，传输文件。
 ```
-python3 images_mover.py --pull -tag-push  --registry-url 192.168.1.123:5000 --image-list-file image_list_file
+python3 images_mover.py --pull -tag-push  --registry-url 192.168.1.123:5000 --image-list-file image_list_file --platforms linux/amd64 linux/arm64
 ```
 ## 镜像列表和私有仓库
 镜像列表和私有仓库的配置通过开关--image-list-file 和--registry-url指定，可以根据自己的需要进行输入。image_list_file中的镜像列表格式如下：
--- images_mover.py
+++ images_mover.py
@@ -1,42 +1,45 @@
 import subprocess
 import argparse
-import docker
 
 def read_image_list(file_path):
     with open(file_path, "r") as f:
         image_list = f.read().splitlines()
     return image_list
 
-def pull_save_as_tar_gz(image_list, save_tar):
+def pull_save_as_tar_gz(image_list, save_tar, platforms):
     # Loop through the list and pull each image
+    image_list_all_platforms = []
     for image_name in image_list:
-        subprocess.run(["docker", "pull", image_name])
+        if platforms:
+            for platform in platforms:
+                platform_tag = platform.split("/")[-1]
+                # Use subprocess to run the docker pull command and pull the image
+                subprocess.run(["docker", "pull", "--platform", platform, image_name])
+                subprocess.run(["docker", "tag", image_name, image_name + "-" + platform_tag])
+                image_list_all_platforms.append(image_name + "-" + platform_tag)
+        else:
+            subprocess.run(["docker", "pull", image_name])
     if save_tar:
-        print("Saving images to image.tar file")
         # Use subprocess to run the docker save command and save the images to a tar file
-        subprocess.run(["docker", "save", "-o", "image.tar"] + image_list)
-        print("Compressing image.tar file")
+        subprocess.run(["docker", "save", "-o", "image.tar"] + image_list if not platforms else ["docker", "save", "-o", "image.tar"] + image_list_all_platforms)
+
         # Compress the tar file using gzip
         subprocess.run(["gzip", "image.tar"])
 
 # file_path is in tar.gz format
-def tag_push_to_registry(image_list, registry_url, file_path="", project_name=None):
+def tag_push_to_registry(image_list, registry_url, file_path="", project_name=None, platforms=None):
     if file_path != "":
-        print("Decompressing image.tar.gz file")
         # Use subprocess to run the gunzip command and decompress the tar.gz file
         subprocess.run(["gunzip", file_path])
 
         # Get the name of the decompressed tar file
         tar_file = file_path[:-3]
-        print("Loading images from " + tar_file + " file")
+
         # Use subprocess to run the docker load command and load the images from the tar file
         subprocess.run(["docker", "load", "-i", tar_file])
-        with open(tar_file, "rb") as tar:
-            determined_image_list = [image.tags[0] for image in client.images.load(tar) if hasattr(image, 'tags') and len(image.tags) > 0]
-    else:
-        determined_image_list = image_list
+
     # Loop through the list of image names and tag and push each image to the specified registry
-    for image_name in determined_image_list:
+    for image_name in image_list:
         # Get the image name without the hostname
         image_name_without_hostname = image_name.split("/")[-1]
 
@@ -45,10 +48,19 @@ def tag_push_to_registry(image_list, registry_url, file_path="", project_name=No
             tagged_image_name = registry_url + "/" + project_name + "/" + image_name_without_hostname
         else:
             tagged_image_name = registry_url + "/" + image_name_without_hostname
-        subprocess.run(["docker", "tag", image_name, tagged_image_name])
-
-        # Push the tagged image to the registry
-        subprocess.run(["docker", "push", tagged_image_name])
+        manifest_all_platforms = []
+        if platforms:
+            for platform in platforms:
+                platform_tag = platform.split("/")[-1]
+                manifest_all_platforms.append(tagged_image_name + "-" + platform_tag)
+                subprocess.run(["docker", "tag", image_name + "-" + platform_tag, tagged_image_name + "-" + platform_tag])
+                subprocess.run(["docker", "push", tagged_image_name + "-" + platform_tag])
+            subprocess.run(["docker", "manifest", "create", "--insecure", "--amend", tagged_image_name] + manifest_all_platforms)
+            subprocess.run(["docker", "manifest", "push", "--insecure", tagged_image_name])
+        else:
+            subprocess.run(["docker", "tag", image_name, tagged_image_name])
+            # Push the tagged image to the registry
+            subprocess.run(["docker", "push", tagged_image_name])
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -58,6 +70,7 @@ parser.add_argument("--pull-save", action="store_true", help="Pull the images an
 parser.add_argument("--project-name", type=str, help="Specify the project name for pushing the images to the registry")
 parser.add_argument("--image-list-file", type=str, help="Specify the file path for reading the image list")
 parser.add_argument("--registry-url", type=str, help="Specify local docker image registry to push to")
+parser.add_argument('--platforms', nargs='+', help='List of architecture platforms to pull and push')
 args = parser.parse_args()
 
 # Check if the --image-list-file argument is set
@@ -74,15 +87,15 @@ if not args.registry_url and args.tag_push:
 
 # Pull the images and save them to a tar file if the --pull-save switch is set
 if args.pull_save:
-    pull_save_as_tar_gz(image_list, True)
+    pull_save_as_tar_gz(image_list, True, args.platforms)
 
 # Pull the images without saving them to a tar file if the --pull switch is set
 elif args.pull:
-    pull_save_as_tar_gz(image_list, False)
+    pull_save_as_tar_gz(image_list, False, args.platforms)
 
 # Tag and push the images to the registry if the --tag-push switch is set
 if args.tag_push:
     if args.pull:
-        tag_push_to_registry(image_list, args.registry_url, project_name=args.project_name)
+        tag_push_to_registry(image_list, args.registry_url, project_name=args.project_name, platforms=args.platforms)
     else:
-        tag_push_to_registry(image_list, args.registry_url, "image.tar.gz", project_name=args.project_name)
+        tag_push_to_registry(image_list, args.registry_url, "image.tar.gz", project_name=args.project_name, platforms=args.platforms)
